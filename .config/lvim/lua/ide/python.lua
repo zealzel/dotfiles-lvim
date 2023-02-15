39e113466e15b55ea2c829ac15b12e147617efb1
zealzel <zealzel@gmail.com>
2023-02-16 00:19:33 +0800

    Uncoment linter ruff in the file python.lua

--- .config/lvim/lua/ide/python.lua
+++ .config/lvim/lua/ide/python.lua
@@ -47,7 +47,7 @@ formatters.setup({
 -- Set a linter.
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
-  { command = "ruff" },
+  -- { command = "ruff" },
   {
     command = "flake8",
     filetypes = {
