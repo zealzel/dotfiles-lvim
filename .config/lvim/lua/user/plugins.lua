9b0383676f910fa7f807962f3a8c63bfd9d7f634
zealzel <zealzel@gmail.com>
2023-06-29 12:58:15 +0800

    Sync and add new pluging: flash (to replace leap)

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -29,12 +29,67 @@ lvim.plugins = {
       require("nvim-surround").setup()
     end,
   },
+  -- {
+  --   -- 🦘 Interface that makes on-screen navigation quicker and more natural than ever
+  --   "ggandor/leap.nvim",
+  --   config = function()
+  --     require("leap").add_default_mappings()
+  --   end,
+  -- },
   {
-    -- 🦘 Interface that makes on-screen navigation quicker and more natural than ever
-    "ggandor/leap.nvim",
-    config = function()
-      require("leap").add_default_mappings()
-    end,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump(
+            {
+              search = {
+                mode = function(str)
+                  return "\\<" .. str
+                end,
+              },
+            }
+          )
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Flash Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
   },
   {
     -- A light-weight lsp plugin based on neovim's built-in lsp with a highly performant UI.
