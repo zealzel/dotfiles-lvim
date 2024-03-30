1d9780dd6471cc0fdc31152f502a47759b5623e3
zealzel <zealzel@gmail.com>
2024-03-30 22:54:10 +0800

    updated. this new branch dev is mainly for environment not in macos

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -39,13 +39,6 @@ lvim.plugins = {
             require("nvim-surround").setup()
         end,
     },
-    -- {
-    --   -- 🦘 Interface that makes on-screen navigation quicker and more natural than ever
-    --   "ggandor/leap.nvim",
-    --   config = function()
-    --     require("leap").add_default_mappings()
-    --   end,
-    -- },
     {
         "folke/flash.nvim",
         event = "VeryLazy",
@@ -116,14 +109,6 @@ lvim.plugins = {
             { "nvim-treesitter/nvim-treesitter" },
         }
     },
-    -- { "adelarsq/vim-devicons-emoji" },
-    -- {
-    --   -- TabNine completion engine for hrsh7th/nvim-cmp
-    --   "tzachar/cmp-tabnine",
-    --   run = "./install.sh",
-    --   requires = "hrsh7th/nvim-cmp",
-    --   event = "InsertEnter",
-    -- },
     -- Seamless tmux/vim navigation (over SSH too!)
     { "sunaku/tmux-navigate" },
     {
@@ -136,13 +121,6 @@ lvim.plugins = {
     "gpanders/editorconfig.nvim",
     -- disables search highlighting when you are done searching and re-enables it when you search again
     "romainl/vim-cool",
-
-    -- Single tabpage interface for easily cycling through diffs for all modified files for any git rev.
-    -- utilize vim's diff mode. Please refer to :h diff.txt
-    -- common usages:
-    --    During merge stage, :DiffviewOpen -> 2do (master) or 3do (branch to merge)
-    --    Find history of one file, :DiffviewFileHistory %
-    --    navigate to next/prev changes -> ]c / [c
     "sindrets/diffview.nvim",
 
     {
@@ -174,43 +152,6 @@ lvim.plugins = {
         config = true,
     },
     "junegunn/vim-easy-align",
-    {
-        "epwalsh/obsidian.nvim",
-        version = "*", -- recommended, use latest release instead of latest commit
-        lazy = true,
-        ft = "markdown",
-        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-        -- event = {
-        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-        --   "BufReadPre path/to/my-vault/**.md",
-        --   "BufNewFile path/to/my-vault/**.md",
-        -- },
-        dependencies = {
-            -- Required.
-            "nvim-lua/plenary.nvim",
-
-            -- see below for full list of optional dependencies 👇
-        },
-        opts = {
-            workspaces = {
-                {
-                    name = "work",
-                    path = "~/vaults/work",
-                },
-                {
-                    name = "personal",
-                    path = "~/vaults/personal",
-                },
-            },
-            templates = {
-                subdir = "Templates",
-                -- date_format = "%Y-%m-%d-%a",
-                -- time_format = "%H:%M",
-            },
-            -- see below for full list of options 👇
-        },
-    },
     {
         "danymat/neogen",
         dependencies = "nvim-treesitter/nvim-treesitter",
@@ -252,28 +193,9 @@ lvim.plugins = {
     "hrsh7th/nvim-cmp",       -- optional, for completion
     "rcarriga/nvim-notify",   -- optional
     "stevearc/dressing.nvim", -- optional, UI for :JupyniumKernelSelect
-    -- {
-    --   -- https://github.com/mg979/vim-visual-multi/issues/241
-    --   "mg979/vim-visual-multi",
-    --   branch = "master",
-    --   init = function()
-    --     vim.g.VM_maps = {
-    --       ["Find Under"] = "<c-m>",
-    --       -- ["Add Cursor Down"] = '<M>j',
-    --       -- ["Add Cursor Up"]   = '<M>k',
-    --     }
-    --   end,
-    -- },
     "nvim-pack/nvim-spectre",
     "kevinhwang91/nvim-bqf",
-    -- "christianchiarulli/harpoon",
     "ThePrimeagen/harpoon",
-    -- {
-    --   "karb94/neoscroll.nvim",
-    --   config = function()
-    --     require('neoscroll').setup()
-    --   end
-    -- },
     "jay-babu/mason-null-ls.nvim",
     {
         "jay-babu/mason-nvim-dap.nvim",
@@ -285,13 +207,6 @@ lvim.plugins = {
             handlers = {}
         },
     },
-    -- { not working
-    --   "max397574/better-escape.nvim",
-    --   config = function()
-    --     require("better_escape").setup({})
-    --   end
-    -- },
-    -- To allow copilot to work with cmp. https://github.com/LunarVim/LunarVim/issues/1856
     {
         "github/copilot.vim",
         -- disable = not lvim.builtin.sell_soul_to_devel,
@@ -312,61 +227,6 @@ lvim.plugins = {
     "junegunn/vim-peekaboo",
     "m00qek/baleia.nvim",
     {
-        "3rd/image.nvim",
-        config = function()
-            require("image").setup({
-                backend = "kitty",
-                integrations = {
-                    markdown = {
-                        enabled = true,
-                        clear_in_insert_mode = false,
-                        download_remote_images = true,
-                        -- only_render_image_at_cursor = false,
-                        only_render_image_at_cursor = true,
-                        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-                    },
-                    neorg = {
-                        enabled = true,
-                        clear_in_insert_mode = false,
-                        download_remote_images = true,
-                        -- only_render_image_at_cursor = false,
-                        only_render_image_at_cursor = true,
-                        filetypes = { "norg" },
-                    },
-                },
-                max_width = nil,
-                max_height = nil,
-                max_width_window_percentage = nil,
-                max_height_window_percentage = 50,
-                window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-                window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-                -- editor_only_render_when_focused = false,                                  -- auto show/hide images when the editor gains/looses focus
-                editor_only_render_when_focused = true,                                   -- auto show/hide images when the editor gains/looses focus
-                tmux_show_only_in_active_window = true,                                   -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-                -- tmux_show_only_in_active_window = false,                                  -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-                hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
-            })
-        end,
-
-    },
-    -- {
-    --     "samodostal/image.nvim",
-    --     config = function()
-    --         require('image').setup {
-    --             render = {
-    --                 min_padding = 5,
-    --                 show_label = true,
-    --                 use_dither = true,
-    --                 foreground_color = false,
-    --                 background_color = false
-    --             },
-    --             events = {
-    --                 update_on_nvim_resize = true,
-    --             },
-    --         }
-    --     end,
-    -- },
-    {
         "jackMort/ChatGPT.nvim",
         event = "VeryLazy",
         config = function()
@@ -379,157 +239,6 @@ lvim.plugins = {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope.nvim"
         }
-    },
-    -- {
-    --   'nosduco/remote-sshfs.nvim',
-    --   config = function()
-    --     require("remote-sshfs").setup({})
-    --   end,
-    -- },
-    {
-        "nvim-neorg/neorg",
-        dependencies = { "luarocks.nvim" },
-        lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-        version = "*", -- Pin Neorg to the latest stable release
-        config = function()
-            require('neorg').setup {
-                configure_parsers = true,
-                install_parsers = true,
-                load = {
-                    ['core.defaults'] = {},
-                    -- ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                    ["core.concealer"] = {
-                        config = {
-                            folds = true,
-                            -- icon_preset = "diamond",
-                            icons = {
-                                code_block = {
-                                    conceal = true,
-                                    width = "content",
-                                    min_width = 85,
-                                },
-                                -- ordered = {
-                                --     icons = { "item 1.", "[0]", "A.", "a.", "§ 1)", "I.", "i.", "⒈", "⑴", "Ⓐ", "ⓐ" },
-                                -- },
-                                -- list = {
-                                --     icons = { "*", " +", "  -", "   >" },
-                                -- },
-                            },
-                        }
-                    },                  -- Adds pretty icons to your documents
-                    ["core.dirman"] = { -- Manages Neorg workspaces
-                        config = {
-                            workspaces = {
-                                notes = "~/notes",
-                                tests = "~/vaults/neorg/tests",
-                            },
-                            default_workspace = "notes",
-                        },
-                    },
-                    ["core.export"] = {},
-                    ["core.export.markdown"] = {},
-                    ["core.summary"] = {
-                        config = {
-                            strategy = "default",
-                        },
-                    },
-                    ["core.completion"] = {
-                        config = {
-                            engine = "nvim-cmp",
-                            name = "[Neorg]",
-                        }
-                    },
-                },
-                highlight = { enable = true, },
-            }
-        end,
-    },
-    -- {
-    --     -- https://www.reddit.com/r/neovim/comments/zxo111/neorg_does_not_install_at_all/
-    --     'nvim-neorg/neorg',
-    --     ft = 'norg',   -- lazy load on filetype
-    --     cmd = 'Neorg', -- lazy load on command, allows you to autocomplete :Neorg regardless of whether it's loaded yet
-    --     --  (you could also just remove both lazy loading things)
-    --     priority = 30, -- treesitter is on default priority of 50, neorg should load after it.
-    --     -- build = ":Neorg sync-parsers", -- This is the important bit!
-    --     config = function()
-    --         require('neorg').setup {
-    --             configure_parsers = true,
-    --             install_parsers = true,
-    --             load = {
-    --                 ['core.defaults'] = {},
-    --                 -- ["core.concealer"] = {}, -- Adds pretty icons to your documents
-    --                 ["core.concealer"] = {
-    --                     config = {
-    --                         folds = true,
-    --                         icon_preset = "diamond"
-    --                     }
-    --                 },                  -- Adds pretty icons to your documents
-
-    --                 ["core.dirman"] = { -- Manages Neorg workspaces
-    --                     config = {
-    --                         workspaces = {
-    --                             notes = "~/notes",
-    --                             tests = "~/vaults/neorg/tests",
-    --                         },
-    --                         default_workspace = "notes",
-    --                     },
-    --                 },
-    --                 ["core.export"] = {},
-    --                 ["core.export.markdown"] = {},
-    --                 ["core.summary"] = {
-    --                     config = {
-    --                         strategy = "default",
-    --                     },
-    --                 },
-    --             },
-    --             highlight = { enable = true, },
-    --         }
-    --     end
-    -- },
-    {
-        "folke/paint.nvim",
-        config = function()
-            local hlmap = {
-                ["^#%s+(.-)%s*$"] = "Operator",
-                ["^##%s+(.-)%s*$"] = "Type",
-                ["^###%s+(.-)%s*$"] = "String",
-                ["^####%s+(.-)%s*$"] = "Constant",
-                ["^#####%s+(.-)%s*$"] = "Number",
-                ["^######%s+(.-)%s*$"] = "Error",
-            }
-            local highlights = {}
-            for pattern, hl in pairs(hlmap) do
-                table.insert(highlights, {
-                    filter = { filetype = "markdown" },
-                    pattern = pattern,
-                    hl = hl,
-                })
-            end
-
-            local hlmap_norg = {
-                ["^%*%s+(.-)%s*$"] = "GruvboxYellow",
-                ["^%*%*%s+(.-)%s*$"] = "GruvboxOrange",
-                ["^%*%*%*%s+(.-)%s*$"] = "GruvboxPurple",
-                ["^%*%*%*%*%s+(.-)%s*$"] = "GruvboxAqua",
-                ["^%*%*%*%*%*%s+(.-)%s*$"] = "GruvboxRed",
-                ["^%*%*%*%*%*%*%s+(.-)%s*$"] = "GruvboxGreen",
-            }
-            local highlights_norg = {}
-            for pattern, hl in pairs(hlmap_norg) do
-                table.insert(highlights_norg, {
-                    filter = { filetype = "norg" },
-                    pattern = pattern,
-                    hl = hl,
-                })
-            end
-
-            require("paint").setup({
-                ---@type PaintHighlight[]
-                -- highlights = highlights,
-                highlights = highlights_norg,
-            })
-        end,
     },
     {
         "folke/todo-comments.nvim",
