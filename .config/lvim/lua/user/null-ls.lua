6a607c8f8ed68d71d56a91e70acbfda40862762d
zealzel <zealzel@gmail.com>
2024-06-30 17:07:03 +0800

    updated

--- .config/lvim/lua/user/null-ls.lua
+++ .config/lvim/lua/user/null-ls.lua
@@ -1,42 +1,42 @@
 -- Set a linter.
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
-  { command = "markdownlint" },
-  { command = "hadolint" },
-  { command = "jsonlint" },
-  { command = "yamllint" },
-  { command = "eslint_d" },
+    { command = "markdownlint" },
+    { command = "hadolint" },
+    { command = "jsonlint" },
+    { command = "yamllint" },
+    { command = "eslint_d" },
 })
-
 
 -- Set a formatter.
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-  -- { command = "remark" },
-  { command = "clang-format" },
-  {
-    command = "shfmt",
-    extra_args = {
-      "--indent=2",
-      "-bn", --binary-next-line Binary ops like && and | may start a line.
-      "-ci", --case-indent Switch cases will be indented.
-      "-sr", --space-redirects Redirect operators will be followed by a space.
-      -- "-kp", --keep-padding Keep column alignment paddings.
-      -- "-fn", --func-next-line Function opening braces are placed on a separate line.
+    -- { command = "remark" },
+    { command = "clang-format" },
+    { command = "stylua" },
+    {
+        command = "shfmt",
+        extra_args = {
+            "--indent=2",
+            "-bn", --binary-next-line Binary ops like && and | may start a line.
+            "-ci", --case-indent Switch cases will be indented.
+            "-sr", --space-redirects Redirect operators will be followed by a space.
+            -- "-kp", --keep-padding Keep column alignment paddings.
+            -- "-fn", --func-next-line Function opening braces are placed on a separate line.
+        },
     },
-  },
-  {
-    command = "prettier",
-    extra_args = {
-      "--no-semi false",
-      "--single-quote true",
-      "--jsx-single-quote true",
+    {
+        command = "prettier",
+        extra_args = {
+            "--no-semi false",
+            "--single-quote true",
+            "--jsx-single-quote true",
+        },
+        filetypes = {
+            "json",
+            "markdown",
+            "yaml",
+            "javascript",
+        },
     },
-    filetypes = {
-      "json",
-      "markdown",
-      "yaml",
-      "javascript",
-    },
-  },
 })
